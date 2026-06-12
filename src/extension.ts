00f5bad5b2e719f335e2874b4147ba18be1bdd19
Gorden Jin <yijin2@andrew.cmu.edu>
2026-06-12 01:08:50 +0000

    feat: detect Python functions using CodeLens

--- src/extension.ts
+++ src/extension.ts
@@ -3,23 +3,80 @@
 import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Your extension is activated as soon as a Python file is opened (see activationEvents in package.json)
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "invariant-based-documentation-generator" is now active!');
+	context.subscriptions.push(
+		vscode.commands.registerCommand('invariant-based-documentation-generator.generator', (functionCode: string = '') => {
+			const panel = vscode.window.createWebviewPanel('IBDGenerator', 'Invariant-Based Documentation Generator', vscode.ViewColumn.Beside, {});
+			panel.webview.html = getWebViewContent(functionCode);
+			panel.onDidDispose(() => {}, null, context.subscriptions);
+		})
+	);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('invariant-based-documentation-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Invariant-Based Documentation Generator!');
-	});
+	context.subscriptions.push(
+		vscode.languages.registerCodeLensProvider({ language: 'python' }, new PythonFunctionCodeLensProvider())
+	);
+}
 
-	context.subscriptions.push(disposable);
+class PythonFunctionCodeLensProvider implements vscode.CodeLensProvider {
+	provideCodeLenses(document: vscode.TextDocument): vscode.CodeLens[] {
+		const lenses: vscode.CodeLens[] = [];
+		const defRegex = /^(\s*)(?:async\s+)?def\s+\w+/;
+
+		for (let i = 0; i < document.lineCount; i++) {
+			const match = document.lineAt(i).text.match(defRegex);
+			if (!match) {
+				continue;
+			}
+
+			const defIndent = match[1].length;
+			let end = i;
+
+			// The function body is every following line indented deeper than the def line
+			for (let j = i + 1; j < document.lineCount; j++) {
+				const line = document.lineAt(j);
+				if (line.isEmptyOrWhitespace) {
+					continue;
+				}
+				if (line.firstNonWhitespaceCharacterIndex <= defIndent) {
+					break;
+				}
+				end = j;
+			}
+
+			const range = new vscode.Range(i, 0, end, document.lineAt(end).text.length);
+			lenses.push(new vscode.CodeLens(new vscode.Range(i, 0, i, 0), {
+				title: 'Generate Invariant-Based Documentation',
+				command: 'invariant-based-documentation-generator.generator',
+				arguments: [document.getText(range)]
+			}));
+		}
+
+		return lenses;
+	}
+}
+
+function escapeHtml(text: string) {
+	return text
+		.replace(/&/g, '&amp;')
+		.replace(/</g, '&lt;')
+		.replace(/>/g, '&gt;');
+}
+
+function getWebViewContent(functionCode: string) {
+	return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invariant-Based Documentation Generator</title>
+</head>
+<body>
+    <h1>Invariant-Based Documentation Generator</h1>
+    <pre>${escapeHtml(functionCode)}</pre>
+</body>
+</html>`;
 }
 
 // This method is called when your extension is deactivated
